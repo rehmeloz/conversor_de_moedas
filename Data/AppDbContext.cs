520590a78d3f33502ad9f69848a508d2c46ff7be
rehmeloz <renatamelo.pro@outlook.com>
2026-09-19 17:41:47 -0300

    Adiciona pasta Data com modelo de banco de dados e ajusta gitignore

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace ConversorMoedas.Data;
+using Microsoft.EntityFrameworkCore;
+using ConversorMoedas.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Moeda> Moedas {  get; set; }
+    public DbSet<Conversao> Conversoes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Moeda>().HasData(
+            new Moeda { Id = 1, Codigo = "USD", Nome = "Dólar Americano", Taxa = 1.0m, UltimaAtualizacao = DateTime.Now },
+            new Moeda { Id = 2, Codigo = "BRL", Nome = "Real Brasileiro", Taxa = 5.0m, UltimaAtualizacao = DateTime.Now },
+            new Moeda { Id = 3, Codigo = "EUR", Nome = "Euro", Taxa = 0.92m, UltimaAtualizacao = DateTime.Now });
+    }
+}
