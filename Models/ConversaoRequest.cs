63d97f9fcf047b457b2689c6d8975639804a7e71
rehmeloz <renatamelo.pro@outlook.com>
2026-09-19 17:25:04 -0300

    Adiciona objeto ConversaoRequest

---
+++ Models/ConversaoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ConversorMoedas.Models;
+
+public class ConversaoRequest
+{
+    public string De { get; set; } = string.Empty;
+    public string Para { get; set; } = string.Empty;
+    public decimal Valor { get; set; }
+}
