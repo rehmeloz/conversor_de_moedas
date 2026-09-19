b4a696482f8feeca2299ddc5253d683a4dee8796
rehmeloz <renatamelo.pro@outlook.com>
2026-09-19 17:47:15 -0300

    Adiciona pasta Services e interface de MoedasService

---
+++ Services/IMoedaService.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversorMoedas.Services;
+using ConversorMoedas.Models;
+
+public interface IMoedaService
+{
+    Task<ConversaoResponse> ConverterMoedaAsync (ConversaoRequest request);
+    Task<List<Moeda>> ListarMoedasAsync();
+    Task AtualizarTaxasAsync();
+    Task<List<Conversao>> ObterHistoricoAsync();
+}
