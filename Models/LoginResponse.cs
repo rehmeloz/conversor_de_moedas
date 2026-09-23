578e1a5967a819ef02654e4725b67f3bb4532e3f
rehmeloz <renatamelo.pro@outlook.com>
2026-09-23 16:12:01 -0300

    Adiciona pasta DTOs; Adiciona novos arquivos para realizar autenticação JWT

---
+++ Models/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversorMoedas.Models;
+using ConversorMoedas.DTOs;
+
+public class LoginResponse
+{
+    public bool Sucesso { get; set; }
+    public string Mensagem { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public UserDto User { get; set; }
+}
